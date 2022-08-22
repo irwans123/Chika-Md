e7e9d8e6dc45d1b3a7a0ec5f10be3b9083aa29c9
irwans123 <102463409+irwans123@users.noreply.github.com>
2022-08-22 15:17:36 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,27 +22,27 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281331998730','6281331998730','6281331998730'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281331998730'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281331998730' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Wanzz' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Xbot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Wanzz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-global.ig = 'https://instagram.com/riychdwayne'
-global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.youtube = ''
+global.ig = ''
+global.mygc = 'https://chat.whatsapp.com/L14r8FqzBxqDJX0kVY7w4Y'
+global.myweb = ''
+global.email = 'irwansyt23@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Xbot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'Wanz' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -73,7 +73,7 @@ global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutp
 
 //—————「 Set Prefix 」—————//
 //gausah di apa² in!
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['#','!','.']
 
 //—————「 Set Simbol 」—————//
 //terserah mai ubah atau nggak, note : tanda ' gausah di hapus!
@@ -82,16 +82,16 @@ global.sp = '⭔'
 //—————「 Set Message 」—————//
 //terserah mau ubah apa nggak, note : tanda ' gausah di hapus!
 global.mess = {
-success: '_Done._',
-admin: '_Fitur Khusus Admin Group!_',
-botAdmin: '_Bot Harus Menjadi Admin Terlebih Dahulu!_',
-owner: '_Fitur Khusus Owner Bot_',
-group: '_Fitur Digunakan Hanya Untuk Group!_',
-private: '_Fitur Digunakan Hanya Untuk Private Chat!_',
-bot: '_Fitur Khusus Pengguna Nomor Bot_',
-wait: '_Sedang Di Proses..._',
-error: '_Fitur Sedang Error!_',
-endLimit: '_Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
+success: '✅ _Done._',
+admin: '🚫 _Fitur Khusus Admin Group!_',
+botAdmin: '🚫 _Bot Harus Menjadi Admin Terlebih Dahulu!_',
+owner: '🚫 _Fitur Khusus Owner Bot_',
+group: '🚫 _Fitur Digunakan Hanya Untuk Group!_',
+private: '🚫 _Fitur Digunakan Hanya Untuk Private Chat!_',
+bot: '🚫 _Fitur Khusus Pengguna Nomor Bot_',
+wait: '⌛ _Sedang Di Proses..._',
+error: '⚠️ _Fitur Sedang Error!_',
+endLimit: '⚠️ _Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12_',
 }
 
 //—————「 Batas Akhir 」—————//
